d8ae14399ddc271711c18040d49420c925298899
Zvat Scorobogatov <zvat.333333@gmail.com>
2020-12-18 20:12:06 +0200

    18.12 - add functions for adding new goods

--- src/Classes/ProductTable.js
+++ src/Classes/ProductTable.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
 
 
 class ProductTable extends React.Component {
+
     state = {
         goods: [
             {id: 1, name: 'Iphone 12 Pro Max', quantity: 30, cost: 1000},
@@ -17,11 +17,49 @@ class ProductTable extends React.Component {
         newGoodsName: '',
         newGoodsCost: '',
         newGoodsQuantity: ''
-    }
+    };
+
+    onChangeName = e => {
+        this.setState( {
+            newGoodsName: e.target.value
+        })
+    };
+
+    onChangeCost = e =>{
+        this.setState({
+            newGoodsCost: +e.target.value
+        })
+    };
+
+    onChangeQuantity = e =>{
+        this.setState({
+            newGoodsQuantity: +e.target.value
+        })
+    };
+
+    addGood = () =>{
+        const { newGoodsName: name , newGoodsCost: cost , newGoodsQuantity: quantity , goods} = this.state;
+        const  newGood = {id: Date.now(), name, cost, quantity };
+
+        if(!newGood.name || !newGood.cost || !newGood.quantity) return alert('Заполните поля');
+
+        this.setState( {
+            goods: [...goods, newGood],
+        });
+        this.resetForm();
+    };
+
+    resetForm = () =>{
+        this.setState( {
+            newGoodsName: '',
+            newGoodsCost: '',
+            newGoodsQuantity: ''
+        });
+    };
 
     render() {
 
-        const { goods } = this.state;
+        const { goods, newGoodsName, newGoodsCost, newGoodsQuantity } = this.state;
 
         return (
             <section>
@@ -33,14 +71,36 @@ class ProductTable extends React.Component {
                                 <p className={"good_price"} style={{ textDecoration: goods.quantity == 0 && 'line-through'}}>ЦЕНА: {goods.cost}$</p>
                                 <p className={"goods_quantity"} style={{ color: goods.quantity == 0 && 'red'}}>Осталось: {goods.quantity}</p>
 
-                                <Button variant={"contained"} disabled={goods.quantity == 0 && true}>Edit</Button>
+                                <Button variant={"contained"}>Изменить</Button>
                             </div>)}
                 </div>
 
                 <div className="form-container">
-                    <form className={"form"}>
-                        <input name="name" type="text"/>
-                    </form>
+                        <input
+                               value={newGoodsName}
+                               onChange={this.onChangeName}
+                               placeholder="Имя"
+                               type="text"/>
+
+                        <br/>
+                        <input
+                               value={newGoodsCost}
+                               onChange={this.onChangeCost}
+                               placeholder="Цена"
+                               type="number"/>
+                        <br/>
+
+                        <input
+                               value={newGoodsQuantity}
+                               onChange={this.onChangeQuantity}
+                               placeholder="Количество"
+                               type="number"/>
+
+
+
+
+                        <br/>
+                        <Button variant={"contained"} onClick={this.addGood}>Добавить товар</Button>
                 </div>
             </section>
         );
